a0c2296106732dec1ea13f9de863fc696f520414
Ajmal Nasumudeen <ajmaln73@gmail.com>
2025-01-09 19:33:07 +0530

    Initial commit for Live Class NoteTaker

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,7 @@
 let mediaRecorder;
 let audioChunks = [];
-const apiKey = "4 U2FsdGVkX18iX1taq3rTsEY8hH2/CUILcPr59RKxmN3hg2YW6o3UyKgK9FmMhnTSBnWpM9Yaa8ElzmZegykWjW/ScMFFaFGBKo9LP9scWKMFPhthlZpELWATtkoywlOVowmtM2NddmFbH6CPHPFjRQ==";
-const url = "https://api.ajmalnasumudeen.in/api/v1/llm/audio";
+const API_ENDPOINT = process.env.API_ENDPOINT;
+const API_KEY = process.env.API_KEY;
 
 document.getElementById("start-recording").addEventListener("click", async () => {
   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
